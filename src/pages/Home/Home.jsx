979ba0e6b903301b5d4b17fb09ef85c6e1edf10e
mohammad <shaikhqaseem769@gmail.com>
2025-01-16 17:20:04 +0530

    component Food Display loaded

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import './Home.css';
 import Header from '../../components/Header/Header.jsx';
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu.jsx'
+
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay.jsx';
 import { useState } from 'react';
 import AppDownload from '../../components/AppDownload/AppDownload.jsx';
